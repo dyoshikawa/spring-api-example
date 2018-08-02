e061c4091724bd9a412e9fa41a950495544b0be7
dyoshikawa <dyoshikawa1993@gmail.com>
2018-08-03 00:06:58 +0900

    インターフェース削除

--- src/main/kotlin/net/dyosh/springapiexample/service/TaskService.kt
+++ src/main/kotlin/net/dyosh/springapiexample/service/TaskService.kt
@@ -2,10 +2,31 @@ package net.dyosh.springapiexample.service
 
 import net.dyosh.springapiexample.entity.Task
 import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import net.dyosh.springapiexample.repository.TaskRepository
 
-interface TaskService {
-    fun findById(id: Long): Task?
-    fun findAll(page: Pageable): List<Task>
-    fun store(task: Task)
-    fun removeById(id: Long)
+@Service
+class TaskService(private val repository: TaskRepository) {
+
+    @Transactional(readOnly = true)
+    fun findById(id: Long): Task? {
+        return repository.findById(id).orElse(null)
+    }
+
+    @Transactional(readOnly = true)
+    fun findAll(page: Pageable): List<Task> {
+        return repository.findAll(page).content
+    }
+
+    @Transactional(timeout = 10)
+    fun store(task: Task) {
+        repository.save(task)
+    }
+
+    @Transactional(timeout = 10)
+    fun removeById(id: Long) {
+        repository.deleteById(id)
+    }
+
 }
